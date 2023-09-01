b589dedf85d41479f40577542872773fb1926986
Djedjij <mat3347571@gmail.com>
2023-09-01 13:42:17 +0300

    01092023

--- src/components/pages/ProductPage.jsx
+++ src/components/pages/ProductPage.jsx
@@ -1,22 +1,29 @@
 import React from "react";
 import { useParams } from "react-router";
-import { items } from "../../utils/consts";
+import { items, itemsLeftMenu } from "../../utils/consts";
+import MainReturnButton from "../../UI/MainReturnButton";
+import LeftMenu from "../../UI/LeftMenu";
 
 const ProductPage = () => {
   const { productId } = useParams();
   const product = items.find((product) => product.id === Number(productId));
   return (
     <div>
-      {product.categories ? (
-        product.categories.map((category) => (
-          <div key={category.id}>
-            <h3>{category.name}</h3>
-            <p>{category.description}</p>
-          </div>
-        ))
-      ) : (
-        <div>Такой категории не существует</div>
-      )}
+      <h2>{product.title}</h2>
+      <MainReturnButton />
+      <div>
+        <LeftMenu arr={itemsLeftMenu} />
+        {product.categories ? (
+          product.categories.map((category) => (
+            <div key={category.id}>
+              <h3>{category.name}</h3>
+              <p>{category.description}</p>
+            </div>
+          ))
+        ) : (
+          <div>Такой категории не существует</div>
+        )}
+      </div>
     </div>
   );
 };
--- src/components/pages/ProductsList.jsx
+++ src/components/pages/ProductsList.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import MainReturnButton from "../../UI/MainReturnButton";
 import LeftMenu from "../../UI/LeftMenu";
 import "../../styles/ProductsList.css";
-import { items, itemsName } from "../../utils/consts";
+import { items, itemsLeftMenu } from "../../utils/consts";
 import { Link } from "react-router-dom";
 const ProductsList = () => {
   return (
@@ -10,7 +10,7 @@ const ProductsList = () => {
       <h2>Каталог продукции</h2>
       <MainReturnButton />
       <div className="product-list">
-        <LeftMenu arr={itemsName} />
+        <LeftMenu arr={itemsLeftMenu} />
         <div className="product-content">
           {items.map((product) => (
             <Link className="product-card" to={`/products/${product.id}`}>
