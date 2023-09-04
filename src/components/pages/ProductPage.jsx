ed5e4bb55c8cb2202b8907fc8049d7d7294c1ec6
Djedjij <mat3347571@gmail.com>
2023-09-04 20:42:31 +0300

    040923

--- src/components/pages/ProductPage.jsx
+++ src/components/pages/ProductPage.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router";
 import { items, itemsLeftMenu } from "../../utils/consts";
 import MainReturnButton from "../../UI/MainReturnButton";
 import LeftMenu from "../../UI/LeftMenu";
-
+import "../../styles/ProductPage.css";
 const ProductPage = () => {
   const { productId } = useParams();
   const product = items.find((product) => product.id === Number(productId));
@@ -11,18 +11,28 @@ const ProductPage = () => {
     <div>
       <h2>{product.title}</h2>
       <MainReturnButton />
-      <div>
+      <div className="product-page">
         <LeftMenu arr={itemsLeftMenu} />
-        {product.categories ? (
-          product.categories.map((category) => (
-            <div key={category.id}>
-              <h3>{category.name}</h3>
-              <p>{category.description}</p>
-            </div>
-          ))
-        ) : (
-          <div>Такой категории не существует</div>
-        )}
+        <div className="product-page-content">
+          {product.categories ? (
+            product.categories.map((category) => (
+              <div className="product-page-card" key={category.id}>
+                <img
+                  className="product-page-card-img"
+                  src="/images/product-icons/1.png"
+                  alt="1"
+                />
+                <div className="product-page-card-text">
+                  <h3>{category.name}</h3>
+                  <hr />
+                  <p>{category.description}</p>
+                </div>
+              </div>
+            ))
+          ) : (
+            <div>Такой категории не существует</div>
+          )}
+        </div>
       </div>
     </div>
   );
