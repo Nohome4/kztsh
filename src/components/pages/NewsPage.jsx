581fd293e166d7f99b416f55377cc7eebe125102
Djedjij <mat3347571@gmail.com>
2023-09-20 18:56:32 +0300

    200923

--- src/components/pages/NewsPage.jsx
+++ src/components/pages/NewsPage.jsx
@@ -4,7 +4,7 @@ import LeftMenu from "../../UI/LeftMenu";
 import "../../styles/News.css";
 import { INFO_ROUTES, news } from "../../utils/consts";
 import { useParams } from "react-router-dom";
-
+import "../../styles/NewsPage.css";
 const NewsPage = () => {
   const { newsId } = useParams();
   const localNews = news.find((localNews) => localNews.id === Number(newsId));
