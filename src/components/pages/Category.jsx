7c6e214d40eb712f56b74babf1229a004905e854
Djedjij <mat3347571@gmail.com>
2023-12-09 12:25:48 +0300

    fix items

--- src/components/pages/Category.jsx
+++ src/components/pages/Category.jsx
@@ -1,70 +1,112 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router";
 import { useSpring, animated } from "react-spring";
 import MainReturnButton from "../../UI/MainReturnButton";
 import SliderComponent from "../../UI/SliderComponent";
 import "../../styles/Category.css";
 import IconsLinks from "../../UI/IconsLinks";
+import ErrorMessage from "../../UI/Error";
+import { fetchItems } from "../../http/allApi";
 
 const Category = () => {
-  const itemsJSON = localStorage.getItem("items");
-  const items = JSON.parse(itemsJSON);
-  const { productId } = useParams();
-  const { categoryId } = useParams();
-
-  const categoryObj = items.find((el) => el.id === Number(productId));
-
-  const item = categoryObj.categories.find(
-    (item) => item.id === Number(categoryId)
-  );
-
+  const { productId, categoryId } = useParams();
+  const [localCategory, setLocalCategory] = useState([]);
+  const [load, setLoad] = useState(false);
+  const [error, setError] = useState(null);
   const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const cachedCategoryData = localStorage.getItem("items");
+    if (cachedCategoryData) {
+      try {
+        setLocalCategory(JSON.parse(cachedCategoryData));
+      } catch (error) {
+        console.error("Error parsing local storage data:", error);
+      }
+    } else {
+      setLoad(true);
+      fetchItems()
+        .then((data) => {
+          setLoad(false);
+          if (data.error) {
+            setError(data.error);
+          } else {
+            setLocalCategory(data);
+            localStorage.setItem("items", JSON.stringify(data));
+          }
+        })
+        .catch((error) => {
+          setLoad(false);
+          setError(error.message);
+        });
+    }
+  }, [categoryId, productId]);
+
   const dropdownAnimation = useSpring({
     opacity: isOpen ? 1 : 0,
     maxHeight: isOpen ? "1000px" : "0px",
     config: {
-      duration: 300, // Длительность анимации
-      delay: isOpen ? 0 : 200, // Задержка перед началом анимации исчезновения
+      duration: 300,
+      delay: isOpen ? 0 : 200,
     },
   });
+
+  const categoryObj = localCategory.find((el) => el.id === Number(productId));
+  const item = categoryObj?.categories.find(
+    (item) => item.id === Number(categoryId)
+  );
+
+  if (!localCategory.length) {
+    return <ErrorMessage message="Нет доступных продуктов." />;
+  }
+
+  if (!item) {
+    return <ErrorMessage message="Продукт не найден." />;
+  }
+
+  if (error) {
+    return <ErrorMessage message={error} />;
+  }
 
   return (
     <div>
       <h2 className="header-h2">{item.name}</h2>
       <MainReturnButton />
-      <div className="category">
-        <div className="category-slider">
-          <SliderComponent images={item.images} />
-        </div>
-        <div className="category-description">
-          <p>{item.description}</p>
-          <div className="category-characteristics">
-            <h3>Характеристики</h3>
-            {item.characteristics.length ? (
-              <table className="category-table">
-                <thead></thead>
-                <tbody>
-                  {item.characteristics.map((el) => (
-                    <tr key={el.name}>
-                      <td>{el.name}</td>
-                      <td>{el.value}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            ) : (
-              <h4 className="category-noInfo">
-                К сожалению у этого товара еще нет описания
-              </h4>
-            )}
+      {!load && (
+        <div className="category">
+          <div className="category-slider">
+            <SliderComponent images={item.images} />
           </div>
-          <div className="product-page-info">
-            Внимание! Кузнечный завод тяжелых штамповок имеет возможность
-            изготовления продукции не предтавленной в этом каталоге, по
-            индивидуальному заказу.
+          <div className="category-description">
+            <p>{item.description}</p>
+            <div className="category-characteristics">
+              <h3>Характеристики</h3>
+              {item.characteristics.length ? (
+                <table className="category-table">
+                  <thead></thead>
+                  <tbody>
+                    {item.characteristics.map((el) => (
+                      <tr key={el.name}>
+                        <td>{el.name}</td>
+                        <td>{el.value}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              ) : (
+                <h4 className="category-noInfo">
+                  К сожалению у этого товара еще нет описания
+                </h4>
+              )}
+            </div>
+            <div className="product-page-info">
+              Внимание! Кузнечный завод тяжелых штамповок имеет возможность
+              изготовления продукции не предтавленной в этом каталоге, по
+              индивидуальному заказу.
+            </div>
           </div>
         </div>
-      </div>
+      )}
       {item.tableCharacteristics.length !== 0 ? (
         <div className="table-characteristics">
           {item.tableCharacteristics && item.tableNameCharacteristics ? (
--- src/components/pages/ProductPage.jsx
+++ src/components/pages/ProductPage.jsx
@@ -1,24 +1,61 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import MainReturnButton from "../../UI/MainReturnButton";
 import LeftMenu from "../../UI/LeftMenu";
 import "../../styles/ProductPage.css";
 import { Link } from "react-router-dom";
+import { fetchItems } from "../../http/allApi";
+import ErrorMessage from "../../UI/Error";
+import Loader from "../../UI/Loader";
 
 const ProductPage = () => {
   const { productId } = useParams();
+  const [localItems, setLocalItems] = useState([]);
+  const [load, setLoad] = useState(false);
+  const [error, setError] = useState(null);
 
-  const itemsJSON = localStorage.getItem("items");
-  const items = JSON.parse(itemsJSON);
+  useEffect(() => {
+    const cachedItemsData = localStorage.getItem("items");
+    if (cachedItemsData) {
+      setLocalItems(JSON.parse(cachedItemsData));
+    } else {
+      setLoad(true);
+      fetchItems()
+        .then((data) => {
+          setLoad(false);
+          if (data.error) {
+            setError(data.error);
+          } else {
+            setLocalItems(data);
+            localStorage.setItem("items", JSON.stringify(data));
+          }
+        })
+        .catch((error) => {
+          setLoad(false);
+          setError(error.message);
+        });
+    }
+  }, [productId]);
 
-  const itemsLeftMenu = items.map((el) => {
-    return {
-      name: el.title,
-      path: `/products/${el.id}`,
-    };
-  });
+  if (!localItems.length) {
+    return <ErrorMessage message="Нет доступных продуктов." />;
+  }
 
-  const product = items.find((product) => product.id === Number(productId));
+  const itemsLeftMenu = localItems.map((el) => ({
+    name: el.title,
+    path: `/products/${el.id}`,
+  }));
+  console.log(itemsLeftMenu);
+  const product = localItems.find(
+    (product) => product.id === Number(productId)
+  );
+  if (!product) {
+    return <ErrorMessage message="Продукт не найден." />;
+  }
+
+  if (error) {
+    return <ErrorMessage message={error} />;
+  }
 
   return (
     <div>
@@ -26,40 +63,43 @@ const ProductPage = () => {
       <MainReturnButton />
       <div className="product-page">
         <LeftMenu arr={itemsLeftMenu} />
-        <div className="product-page-content">
-          {product.categories ? (
-            product.categories.map((category) => (
-              <div className="product-page-card" key={category.id}>
-                <img
-                  className="product-page-card-img"
-                  src={process.env.REACT_APP_API_URL + category.images[0]}
-                  alt={category.name}
-                />
-                <div className="product-page-card-text">
-                  <Link to={`/products/${product.id}/${category.id}`}>
-                    <h3>{category.name}</h3>
-                  </Link>
-                  <hr />
-                  <p>{category.description}</p>
-
-                  {category.characteristics &&
-                    category.characteristics.map((el) => (
-                      <div
-                        className="product-page-card-characteristics-wrapper"
-                        key={el.id}
-                      >
-                        <p className="product-page-card-characteristics">
-                          {el.name} - {el.value}
-                        </p>
-                      </div>
-                    ))}
+        {load ? (
+          <Loader />
+        ) : (
+          <div className="product-page-content">
+            {product.categories ? (
+              product.categories.map((category) => (
+                <div className="product-page-card" key={category.id}>
+                  <img
+                    className="product-page-card-img"
+                    src={process.env.REACT_APP_API_URL + category.images[0]}
+                    alt={category.name}
+                  />
+                  <div className="product-page-card-text">
+                    <Link to={`/products/${product.id}/${category.id}`}>
+                      <h3>{category.name}</h3>
+                    </Link>
+                    <hr />
+                    <p>{category.description}</p>
+                    {category.characteristics &&
+                      category.characteristics.map((el) => (
+                        <div
+                          className="product-page-card-characteristics-wrapper"
+                          key={el.id}
+                        >
+                          <p className="product-page-card-characteristics">
+                            {el.name} - {el.value}
+                          </p>
+                        </div>
+                      ))}
+                  </div>
                 </div>
-              </div>
-            ))
-          ) : (
-            <div>Такой категории не существует</div>
-          )}
-        </div>
+              ))
+            ) : (
+              <div>Такой категории не существует</div>
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
